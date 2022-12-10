f9ab35473d86e485f43cf615c53cc9d06b263b49
stav zilber <93096648+stavzilber21@users.noreply.github.com>
2022-12-10 20:02:38 +0200

    Add files via upload

--- src/main/java/com/example/chic_it_app/PostActivity.java
+++ src/main/java/com/example/chic_it_app/PostActivity.java
@@ -10,32 +10,42 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
-
-import com.example.chic_it_app.databinding.ActivityPostBinding;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
+import com.google.firebase.storage.StorageTask;
+import java.util.HashMap;
+
 
 public class PostActivity extends AppCompatActivity {
     ImageView close;
-    ActivityPostBinding binding;
+    ImageView imageAdded;
     Uri imageUri;
     StorageReference storageReference;
     ProgressDialog progressDialog;
+    String imageUrl;
+    EditText description;
+    TextView post;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityPostBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_post);
         close = findViewById(R.id.close);
+        imageAdded = findViewById(R.id.image_added);
+        post = findViewById(R.id.post);
+        description = findViewById(R.id.description);
 
         close.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,14 +55,14 @@ public class PostActivity extends AppCompatActivity {
             }
         });
 
-        binding.selectImagebtn.setOnClickListener(new View.OnClickListener() {
+        imageAdded.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 selectImage();
             }
         });
 
-        binding.uploadimagebtn.setOnClickListener(new View.OnClickListener() {
+        post.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 uploadImage();
@@ -61,6 +71,58 @@ public class PostActivity extends AppCompatActivity {
 
     }
 
+    private void uploadImage() {
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Uploading");
+        progressDialog.show();
+
+
+        if (imageUri != null){
+            storageReference = FirebaseStorage.getInstance().getReference("Posts").child(System.currentTimeMillis() + "." + getFileExtension(imageUri));
+            StorageTask uploadtask = storageReference.putFile(imageUri);
+            uploadtask.continueWithTask(new Continuation() {
+                @Override
+                public Object then(@NonNull Task task) throws Exception {
+                    if (!task.isSuccessful()){
+                        throw task.getException();
+                    }
+
+                    return storageReference .getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    Uri downloadUri = task.getResult();
+                    imageUrl = downloadUri.toString();
+
+                    DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Posts");
+                    String postId = ref.push().getKey();
+
+                    HashMap<String , Object> map = new HashMap<>();
+                    map.put("postid" , postId);
+                    map.put("imageurl" , imageUrl);
+                    map.put("description" , description.getText().toString());
+//                    map.put("publisher" , FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+                    ref.child(postId).setValue(map);
+
+                    progressDialog.dismiss();
+                    Toast.makeText(PostActivity.this, "The post has been uploaded!", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(PostActivity.this , CreatingcontentActivity.class));
+                    finish();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(PostActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+    } else {
+        Toast.makeText(this, "No image was selected!", Toast.LENGTH_SHORT).show();
+    }
+
+    }
     private void selectImage() {
 
         Intent intent = new Intent();
@@ -69,46 +131,11 @@ public class PostActivity extends AppCompatActivity {
         startActivityForResult(intent,100);
 
     }
+    private String getFileExtension(Uri uri) {
 
-    private void uploadImage() {
-
-        progressDialog = new ProgressDialog(this);
-        progressDialog.setTitle("Uploading File....");
-        progressDialog.show();
-
-
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.CANADA);
-        Date now = new Date();
-        String fileName = formatter.format(now);
-        storageReference = FirebaseStorage.getInstance().getReference("images/"+fileName);
-
-
-        storageReference.putFile(imageUri)
-                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-
-                        binding.firebaseimage.setImageURI(null);
-                        Toast.makeText(PostActivity.this,"Successfully Uploaded",Toast.LENGTH_SHORT).show();
-                        if (progressDialog.isShowing())
-                            progressDialog.dismiss();
-
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-
-
-                        if (progressDialog.isShowing())
-                            progressDialog.dismiss();
-                        Toast.makeText(PostActivity.this,"Failed to Upload",Toast.LENGTH_SHORT).show();
-
-
-                    }
-                });
+        return MimeTypeMap.getSingleton().getExtensionFromMimeType(this.getContentResolver().getType(uri));
 
     }
-
 
 
     @Override
@@ -118,9 +145,13 @@ public class PostActivity extends AppCompatActivity {
         if (requestCode == 100 && data != null && data.getData() != null){
 
             imageUri = data.getData();
-            binding.firebaseimage.setImageURI(imageUri);
-
-
+            imageAdded.setImageURI(imageUri);
+        }
+        else {
+            Toast.makeText(this, "Try again!", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(PostActivity.this , CreatingcontentActivity.class));
+            finish();
         }
     }
+
 }
